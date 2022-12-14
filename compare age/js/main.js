c64f63a02b41cf28fa4164ee4fcb04cc0a7bf6e3
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-14 08:57:13 +0530

    conditional statement.

---
+++ compare age/js/main.js
@@ -0,0 +1,15 @@
+var a=parseInt(prompt("enter a age"));
+var b=parseInt(prompt("enter b age"));
+var c=parseInt(prompt("enter c age"));
+if (a>b && a>c)
+{
+console.log("a is OLDER");
+}
+else if (b>a && b>c)
+{
+console.log("b is OLDER");
+}
+else
+{
+console.log("c is OLDER");
+}	
